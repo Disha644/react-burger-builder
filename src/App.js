92432e917723ec12c4f0fc0386d6a114d0d9cb1c
Disha644 <dgupta644@gmail.com>
2021-01-13 12:17:04 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './App.css';
+
+function App() {
+  return (
+    <div className={classes.App}>
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
